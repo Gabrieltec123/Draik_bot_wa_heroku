eab0fd84c854efb85fd26445ed1dc366d6a16b3d
GabrielTec_735 <74263351+Gabrieltec123@users.noreply.github.com>
2021-10-10 23:11:57 -0500

    Add files via upload

---
+++ queenamdi/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2020 Draik_bot - GabrielTec.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Draik_bot - GabrielTec
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
